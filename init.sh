c859f8cb10d95d37b5bbf2449693e3caadbf5a55
hamed-448 <40559510+hamed-448@users.noreply.github.com>
2018-06-25 09:59:02 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+
+#TZ="Europe/Amsterdam"
+
+#echo $TZ > /etc/timezone
+#cp /usr/share/zoneinfo/$TZ /etc/localtime
+
 PATTERNS="jdk*
 sqldeveloper*"
 
 cd /opt
 
 for pattern in $PATTERNS; do
-	compfile=$(find /tmp -iname $pattern | tail -1)
-	if ( echo $compfile | grep -iq .tar.gz ) && ( ! ls -1  /opt | grep -iq $pattern ) ; then
-		tar -zxvf $compfile 
-	fi
-	if ( echo $compfile | grep -iq zip ) && ( ! ls -1 /opt | grep -iq $pattern) ; then
-		unzip $compfile
-	fi
+        compfile=$(find /tmp -iname $pattern | sort | tail -1)
+        if ( echo $compfile | grep -iq .tar.gz ) && ( ! ls -1  /opt | grep -iq $pattern ) ; then
+                tar -zxvf $compfile 
+        fi
+        if ( echo $compfile | grep -iq zip ) && ( ! ls -1 /opt | grep -iq $pattern) ; then
+                unzip $compfile
+        fi
 done
 
 export JAVA_HOME="$(find /opt -maxdepth 1 -iname 'jdk*' | sort | tail -n1)/"
